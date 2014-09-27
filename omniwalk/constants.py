ff6e8bfc3b3db6e8b096b2cdf8d61bff20b9cd3e
Rodrigo da Silva Guerra <rodrigo.guerra@ufsm.br>
2014-09-27 13:08:15 -0300

    Created a separate module for the omniwalk classes

---
+++ omniwalk/constants.py
@@ -0,0 +1,39 @@
+# Configuration parameters used by the gait
+# generation algorithm.
+
+C1 = 0.02 # Halt Position Leg Extension
+C2 = 0.1 # Halt Position Leg Roll Angle
+C3 = 0.02 # Halt Position Leg Pitch Angle
+C4 = 0.03 # Halt Position Foot Roll Angle
+C5 = 0.0 # Halt Position Foot Pitch Angle
+
+C6 = 0.02 # Constant Ground Push
+C7 = 0.0 # Proportional Ground Push
+C8 = 0.3 # Constant Step Height
+C9 = 0.12 # Proportional Step Height
+
+C_tau_0 = 0 # Swing Start Timing
+C_tau_1 = 2.3876 # Swing Stop Timing
+C10 = 0.17 # Sagittal Swing Amplitude Fwd
+C11 = 0.12 # Sagittal Swing Amplitude Bwd
+C12 = 0.1 # Lateral Swing Amplitude
+C13 = 0.05 # Lateral Swing Amplitude Offset
+C14 = 0.015 # Turning Lateral Swing Amplitude Offset
+C15 = 0.2 # Rotational Swing Amplitude
+C16 = 0.05 # Rotational Swing Amplitude Offset
+
+C17 = 0.035 # Lateral Hip Swing Amplitude
+
+C18 = 0 # Forward Lean
+C19 = 0 # Backward Lean
+C20 = -0.07 # Forward and Turning Lean
+
+C21 = 3.5 # Gait Velocity Limiting Norm p
+C22 = 0.0085 # Sagittal Acceleration
+C23 = 0.01 # Lateral Acceleration
+C24 = 0.009 # Rotational Acceleration
+
+C25 = 0.09 # Constant Step Frequency
+C26 = 0.008 # Sagittal Proportional Step Frequency
+C27 = 0 # Lateral Proportional Step Frequency
+
