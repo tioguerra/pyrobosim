649cc6be1f71d005132cc75d81cadc0fa93273ac
Marcelo Canaparro <marcelocanaparro@gmail.com>
2014-10-12 23:59:07 -0300

    adjusted frequency feel more like NimbRO

--- omniwalk/constants.py
+++ omniwalk/constants.py
@@ -33,7 +33,7 @@ C22 = 0.0035 # Sagittal Acceleration
 C23 = 0.01 # Lateral Acceleration
 C24 = 0.009 # Rotational Acceleration
 
-C25 = 0.065 # Constant Step Frequency  0.065
+C25 = 0.12 # Constant Step Frequency  0.065
 C26 = 0.008 # Sagittal Proportional Step Frequency
 C27 = 0 # Lateral Proportional Step Frequency
 
