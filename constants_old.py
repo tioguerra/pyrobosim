e1aadaf2daa3479edfb2a7e1c122fea68f3e9094
Marcelo Canaparro <marcelocanaparro@gmail.com>
2014-10-12 22:52:34 -0300

    saved working constants to constants_old.py

---
+++ constants_old.py
@@ -0,0 +1,41 @@
+# Configuration parameters used by the gait
+# generation algorithm.
+
+C1 = 0.03 # Halt Position Leg Extension
+C2 = 0.10 # Halt Position Leg Roll Angle
+C3 = 0.055 # Halt Position Leg Pitch Angle
+C4 = 0.03 # Halt Position Foot Roll Angle
+C5 = 0.01 # Halt Position Foot Pitch Angle -0.025
+
+C6 = 0.005 # Constant Ground Push  0.005
+C7 = 0.0 # Proportional Ground Push
+C8 = 0.05 # Constant Step Height
+C9 = 0.02 # Proportional Step Height
+
+C_tau_0 = 0 # Swing Start Timing
+C_tau_1 = 2.3876 # Swing Stop Timing
+C10 = 0.14 # Sagittal Swing Amplitude Fwd
+C11 = 0.12 # Sagittal Swing Amplitude Bwd
+C12 = 0.1 # Lateral Swing Amplitude
+C13 = 0.05 # Lateral Swing Amplitude Offset
+C14 = 0.015 # Turning Lateral Swing Amplitude Offset
+C15 = 0.2 # Rotational Swing Amplitude
+C16 = 0.05 # Rotational Swing Amplitude Offset
+
+C17 = 0.020 # Lateral Hip Swing Amplitude
+
+C18 = 0.03 # Forward Lean
+C19 = 0 # Backward Lean
+C20 = -0.07 # Forward and Turning Lean
+
+C21 = 3.5 # Gait Velocity Limiting Norm p
+C22 = 0.0035 # Sagittal Acceleration
+C23 = 0.01 # Lateral Acceleration
+C24 = 0.009 # Rotational Acceleration
+
+C25 = 0.065 # Constant Step Frequency  0.065
+C26 = 0.008 # Sagittal Proportional Step Frequency
+C27 = 0 # Lateral Proportional Step Frequency
+
+C28 = 0.002 # Start motion increment
+
