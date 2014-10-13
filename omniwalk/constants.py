b768d3dcbf1fa88df3f527771a678823e0e7557f
Marcelo Canaparro <marcelocanaparro@gmail.com>
2014-10-12 23:05:18 -0300

    changed a few constants

--- omniwalk/constants.py
+++ omniwalk/constants.py
@@ -1,20 +1,20 @@
 # Configuration parameters used by the gait
 # generation algorithm.
 
-C1 = 0.02 # Halt Position Leg Extension
+C1 = 0.03 # Halt Position Leg Extension
 C2 = 0.1 # Halt Position Leg Roll Angle
-C3 = 0.02 # Halt Position Leg Pitch Angle
+C3 = 0.055 # Halt Position Leg Pitch Angle
 C4 = 0.03 # Halt Position Foot Roll Angle
-C5 = 0.0 # Halt Position Foot Pitch Angle
+C5 = 0.01 # Halt Position Foot Pitch Angle -0.025
 
-C6 = 0.02 # Constant Ground Push
+C6 = 0.005 # Constant Ground Push  0.005
 C7 = 0.0 # Proportional Ground Push
-C8 = 0.3 # Constant Step Height
-C9 = 0.12 # Proportional Step Height
+C8 = 0.05 # Constant Step Height
+C9 = 0.02 # Proportional Step Height
 
 C_tau_0 = 0 # Swing Start Timing
 C_tau_1 = 2.3876 # Swing Stop Timing
-C10 = 0.17 # Sagittal Swing Amplitude Fwd
+C10 = 0.14 # Sagittal Swing Amplitude Fwd
 C11 = 0.12 # Sagittal Swing Amplitude Bwd
 C12 = 0.1 # Lateral Swing Amplitude
 C13 = 0.05 # Lateral Swing Amplitude Offset
@@ -22,18 +22,20 @@ C14 = 0.015 # Turning Lateral Swing Amplitude Offset
 C15 = 0.2 # Rotational Swing Amplitude
 C16 = 0.05 # Rotational Swing Amplitude Offset
 
-C17 = 0.035 # Lateral Hip Swing Amplitude
+C17 = 0.020 # Lateral Hip Swing Amplitude
 
-C18 = 0 # Forward Lean
+C18 = 0.03 # Forward Lean
 C19 = 0 # Backward Lean
 C20 = -0.07 # Forward and Turning Lean
 
 C21 = 3.5 # Gait Velocity Limiting Norm p
-C22 = 0.0085 # Sagittal Acceleration
+C22 = 0.0035 # Sagittal Acceleration
 C23 = 0.01 # Lateral Acceleration
 C24 = 0.009 # Rotational Acceleration
 
-C25 = 0.03 # Constant Step Frequency
+C25 = 0.065 # Constant Step Frequency  0.065
 C26 = 0.008 # Sagittal Proportional Step Frequency
 C27 = 0 # Lateral Proportional Step Frequency
+
+C28 = 0.002 # Start motion increment
 
