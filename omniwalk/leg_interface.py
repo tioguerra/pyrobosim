9a0a75f6dbe2341f629ef2ccac4cc7a112af2c25
Marcelo Canaparro <marcelocanaparro@gmail.com>
2014-10-12 23:02:17 -0300

    added leg sign to correct output angles

--- omniwalk/leg_interface.py
+++ omniwalk/leg_interface.py
@@ -4,7 +4,7 @@ from numpy import *
 class LegInterface:
     def __init__(self):
         pass
-    def joint_angles(self, angles):
+    def joint_angles(self, angles, sigma):
         legYaw = angles['legYaw']
         a = cos(-legYaw)
         b = -sin(-legYaw)
@@ -17,10 +17,10 @@ class LegInterface:
         pitch_leg, roll_leg = dot(R,theta).transpose()[0].tolist()
         lamda = arccos(1.0 - angles['eta'])
         jointAngles = {'hipYaw':legYaw,\
-                       'hipRoll':roll_leg,\
+                       'hipRoll':sigma*roll_leg,\
                        'hipPitch':pitch_leg-lamda,\
                        'knee':2*lamda,\
                        'anklePitch':angles['footPitch']-pitch_leg-lamda,\
-                       'ankleRoll':angles['footRoll']-roll_leg}
+                       'ankleRoll':angles['footRoll']-sigma*roll_leg} #added sigma to return the correct value
         return jointAngles
 
