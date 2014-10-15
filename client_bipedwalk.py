84d2b854dd986898c05e638c553e86114e3c2378
Marcelo Canaparro <marcelocanaparro@gmail.com>
2014-10-15 00:13:45 -0300

    pygame now shows the robot's speed

--- client_bipedwalk.py
+++ client_bipedwalk.py
@@ -16,8 +16,12 @@ c = UDPCtrlClient(sys.argv[1], sys.argv[2], sys.argv[3])
 
 # Init PyGame
 pygame.init()
-pygame.display.set_mode((150,150))
-screen = pygame.display.get_surface()
+screen = pygame.display.set_mode((250,250))
+pygame.display.set_caption('Robot Control')
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((250,250,250))
+font = pygame.font.SysFont("arial", 24)
 
 #Init walking modules
 control = ControlInterface()
@@ -33,29 +37,33 @@ while not quit:
     for e in pygame.event.get():
         if e.type == QUIT:
             quit = True
+        #defines keyboard velocity controls
         elif e.type == KEYDOWN:
             if e.key == K_w:
-                print "increase forward speed"
                 VX = VX + 0.025
             if e.key == K_s:
-                print "decrease forward speed"
                 VX = VX - 0.025
             if e.key == K_a:
-                print "increase lateral speed"
                 VY = VY + 0.025
             if e.key == K_d:
-                print "decrease lateral speed"
                 VY = VY - 0.025
             if e.key == K_q:
-                print "decrease rotational speed"
                 VPHI = VPHI + 0.025
             if e.key == K_e:
-                print "decrease rotational speed"
                 VPHI = VPHI - 0.025
 
+    #renders the velocities to the pygame screen
+    VXText = font.render("VX = %1.3f" %VX, True, (10,10,10))
+    VYText = font.render("VY = %1.3f" %VY, True, (10,10,10))
+    VPHIText = font.render("VPHI = %1.3f" %VPHI, True, (10,10,10))
+    screen.fill((255,255,255))
+    screen.blit(VXText,(screen.get_width()/2-VXText.get_width()/2,screen.get_width()/6-VXText.get_height()/2))
+    screen.blit(VYText,(screen.get_width()/2-VYText.get_width()/2,3*screen.get_width()/6-VYText.get_height()/2))
+    screen.blit(VPHIText,(screen.get_width()/2-VPHIText.get_width()/2,5*screen.get_width()/6-VPHIText.get_height()/2))
+    
     Vx,Vy,Vphi,tau,start = control.full_control(VX,VY,VPHI)
     # calculates angles for the left leg
-    angles = motion.final_motion(Vx,-Vy,-Vphi, tau, 1, start)
+    angles = motion.final_motion(Vx,Vy,-Vphi, tau, 1, start)
     rightJointAngles = leg.joint_angles(angles,1)
     # calculates angles for the right leg
     angles = motion.final_motion(Vx,Vy,Vphi,(tau+2*pi) % (2*pi) - pi, -1, start)
@@ -76,7 +84,6 @@ while not quit:
                'lAnkleTilt':-leftJointAngles['anklePitch'],\
                'rAnkleTilt':-rightJointAngles['anklePitch'],\
                }
-    print command
     # sends the desired target angles to server, and
     # receive back in r all the current sensor readings
     r = c.sendCommand(command)
