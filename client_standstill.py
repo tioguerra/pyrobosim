efaae471f0ee2d30d4f63bbd2b7eae92f0447c4a
Marcelo Canaparro <marcelocanaparro@gmail.com>
2015-02-05 18:36:15 -0200

    deleted a bunch of old unused angles

--- client_standstill.py
+++ client_standstill.py
@@ -11,11 +11,8 @@ c = UDPCtrlClient(sys.argv[1], sys.argv[2], sys.argv[3])
 
 initialTargets = {'lAnkleRoll': 0.0, 'rKnee': 0.0, 'rAnkleTilt':0.0, 'lAnkleTilt': 0.0, 'lHipRoll': 0.0,'rHipRoll': 0.1, 'lHipTilt': 0.18033484232311969, 'lKnee':0.0, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': 0.0,'rHipTilt': 0.0}
 
-#finalTargets = {'lAnkleRoll': -0.07, 'rKnee': -0.40066968464623937, 'rAnkleTilt':0.23033484232311968, 'lAnkleTilt': 0.23033484232311968, 'lHipRoll': -0.1,'rHipRoll': 0.1, 'lHipTilt': 0.18033484232311969, 'lKnee':-0.40066968464623937, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.07,'rHipTilt': 0.18033484232311969}
-#finalTargets = {'lAnkleRoll': -0.03996001083571739, 'rKnee': -0.40066968464623937, 'rAnkleTilt': 0.2453348423231197, 'lAnkleTilt': 0.2453348423231197, 'lHipRoll': 0.06996001083571739, 'rHipRoll': 0.06996001083571739, 'lHipTilt': 0.18033484232311969, 'lKnee': -0.40066968464623937, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.03996001083571739, 'rHipTilt': 0.18033484232311969}
-#finalTargets = {'lAnkleRoll': -0.03714363112267023, 'rKnee': -0.40066968464623937, 'rAnkleTilt': 0.2453348423231197, 'lAnkleTilt': 0.2453348423231197, 'lHipRoll': 0.06714363112267023, 'rHipRoll': 0.06714363112267023, 'lHipTilt': 0.18033484232311969, 'lKnee': -0.40066968464623937, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.03714363112267023, 'rHipTilt': 0.18033484232311969}
-finalTargets = {'lAnkleRoll': -0.04003067464806073, 'rKnee': -0.49118446938974419, 'rAnkleTilt': 0.26559223469487209, 'lAnkleTilt': 0.26556391439394639, 'lHipRoll': 0.07003067464806073, 'rHipRoll': 0.06996932535193928, 'lHipTilt': 0.22556391439394641, 'lKnee': -0.4911278287878928, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.03996932535193928, 'rHipTilt': 0.22559223469487211}
-#finalTargets = {'lAnkleRoll': -0.029969007926221113, 'rKnee': -0.491192675166647, 'rAnkleTilt': 0.2455963375833235, 'lAnkleTilt': 0.24556366819068598, 'lHipRoll': 0.05996900792622111, 'rHipRoll': 0.05996900792622111, 'lHipTilt': 0.24556366819068598, 'lKnee': -0.49112733638137196, 'rHipYaw': 0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.029969007926221113, 'rHipTilt': 0.2455963375833235}
+finalTargets = {'lAnkleRoll': -0.07002828646836044, 'rKnee': -0.49131053823344362, 'rAnkleTilt': 0.29065526911672179, 'lAnkleTilt': 0.29055654058733033, 'lHipRoll': 0.10002828646836044, 'rHipRoll': 0.09997171353163957, 'lHipTilt': 0.19055654058733035, 'lKnee': -0.49111308117466068, 'rHipYaw': -0.0, 'lHipYaw': 0.0, 'rAnkleRoll': -0.06997171353163957, 'rHipTilt': 0.19065526911672181}
+
 steps = 250
 
 for step in range(steps):
