3a5673f3f996d52eab0e62482b16c317c21053d8
Marcelo Canaparro <marcelocanaparro@gmail.com>
2015-02-05 18:38:23 -0200

    added the state estimation module

--- omniwalk/__init__.py
+++ omniwalk/__init__.py
@@ -1,5 +1,6 @@
 from control_interface import ControlInterface
 from leg_interface import LegInterface
 from motion_pattern import MotionPattern
+from state_estimation import StateEstimation
 from constants import *
 
