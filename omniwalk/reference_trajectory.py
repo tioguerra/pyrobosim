1e85e85568378eee2d3499ccecac427f551d54bc
Marcelo Canaparro <marcelocanaparro@gmail.com>
2015-02-05 18:40:52 -0200

    changed a few datails on reference trajectory module

--- omniwalk/reference_trajectory.py
+++ omniwalk/reference_trajectory.py
@@ -1,23 +1,22 @@
 from math import *
+from constants import *
 
-class reference_tejectory:
-    def __init__(self,apha,delta,omega,sigma,C):
-        self.alpha = alpha
-        self.delta = delta
-        self.omega = omega
-        self.sigma = sigma
-        self.C = C
+class reference_trajectory:
+    def __init__(self):
+        pass
     def exchange_state(Vx,Vy,Vphi,lamda):
         norm = sqrt(Vx**2 + Vy**2 + Vphi**2)
         Vx = Vx/norm
         Vy = Vy/norm
         Vphi = Vphi/norm
 
-        ksi = self.delta + abs(Vy)*(omega - delta)
+        ksi = delta + abs(Vy)*(omega - delta)
         tau = (1/C) * log((ksi/alpha) + sqrt((ksi**2)/(alpha**2) - 1))
 
+        #definition of a target state
         Sx = Vx*(sigma/C)*sinh(C*tau)
-        Sx_vel = Vx*sigma*cosh(C*tau)
+        Sx_dot = Vx*sigma*cosh(C*tau)
+        
         if abs(Vy) == Vy:
             if lamda > 0:
                 Sy = lamda * ksi
@@ -28,5 +27,6 @@ class reference_tejectory:
                 Sy = lamda * ksi
             else:
                 Sy = lamda * delta
-        Sy_vel = lamda*C*sqrt((Sy**2) - (alpha**2))
-        return Sx, Sx_vel, Sy, Sy_vel
+
+        Sy_dot = lamda*C*sqrt((Sy**2) - (alpha**2))
+        return Sx, Sx_dot, Sy, Sy_dot
